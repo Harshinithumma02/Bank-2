192701b74343f729bd4d85093a8272d37be3c7ab
Harshinithumma02 <73868560+Harshinithumma02@users.noreply.github.com>
2020-11-24 09:10:09 +0530

    Add files via upload
    Component

--- Bank2/src/main/java/com/Areteans/Bank2/service/AccountRowMapper.java
+++ Bank2/src/main/java/com/Areteans/Bank2/service/AccountRowMapper.java
@@ -2,16 +2,19 @@ package com.Areteans.Bank2.service;
 
 import com.Areteans.Bank2.models.Account;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public final class AccountRowMapper implements RowMapper<Account> {
     public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
         Account acc = new Account();
         acc.setUsername(rs.getString("username"));
         acc.setAccType(rs.getString("acc_type"));
         acc.setAcc_id(rs.getInt("acc_id"));
+        acc.setBalance(rs.getDouble("balance"));
         return acc;
     }
 }
