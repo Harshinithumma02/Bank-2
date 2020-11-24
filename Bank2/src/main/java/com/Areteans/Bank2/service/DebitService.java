f2d3e4bd1dd5016004269abefde575db8f251a35
Harshinithumma02 <73868560+Harshinithumma02@users.noreply.github.com>
2020-11-24 09:11:28 +0530

    Add files via upload
    Updated Debitservice

--- Bank2/src/main/java/com/Areteans/Bank2/service/DebitService.java
+++ Bank2/src/main/java/com/Areteans/Bank2/service/DebitService.java
@@ -2,12 +2,21 @@ package com.Areteans.Bank2.service;
 
 import com.Areteans.Bank2.models.Account;
 import com.Areteans.Bank2.models.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
     public class DebitService {
-//        public Account debit(Transaction tx, JdbcTemplate jt) {
+//    private final AccountRowMapper aMap;
+//
+//    public DebitService(AccountRowMapper aMap) {
+//        this.aMap = aMap;
+//    }
+    @Autowired
+    AccountRowMapper aMap;
+
+    //        public Account debit(Transaction tx, JdbcTemplate jt) {
 //            Connection connection = PostgresManager.getConnection();
 //            Account account=new Account();
 //            try {
@@ -38,30 +47,29 @@ import org.springframework.stereotype.Service;
 //            return account;
 //        }
 public Account postDebit(Transaction tx, JdbcTemplate jt) {
-    Integer oldBal=jt.queryForObject("select (balance) from account where acc_id=?",new Object[]{tx.getAmt()},Integer.class);
+    Integer oldBal=jt.queryForObject("select (balance) from account where acc_id=?",Integer.class,tx.getAmt());
     int count=jt.update("update account set balance=? where acc_id=?",tx.getAmt()+(oldBal.intValue()),tx.getAcc_id());
-    Account account=jt.queryForObject("select * from account where acc_id=?",new Object[]{tx.getAcc_id()},Account.class);
+    Account account=jt.queryForObject("select * from account where acc_id=?",Account.class,tx.getAcc_id());
     return account;
 }
     public Account getDebit( int acc_id,JdbcTemplate jt) {
         Account account=jt.queryForObject("select * from account where acc_id=?",
-                new Object[] {acc_id},new AccountRowMapper());
+                aMap,acc_id);
         return account;
     }
-    public Account putDebit(Account acc, JdbcTemplate jt) {
-        Account account=jt.queryForObject("insert into account values (?,?,?,?,?)",
-                Account.class,
-                acc.getAccType(),
+    public int putDebit(Account acc, JdbcTemplate jt) {
+        int count=jt.update("insert into account values (?,?,?,?,?)",
                 acc.getAcc_id(),
+                acc.getAccType(),
                 acc.getBalance(),
                 acc.getUsername(),
                 acc.getPassword());
-        return account;
+        return count;
     }
-    public Account deleteDebit(Account acc, JdbcTemplate jt) {
-        Account account=jt.queryForObject("delete from account where acc_id=?",
-                Account.class, acc.getAcc_id());
-        return account;
+    public int deleteDebit(Account acc, JdbcTemplate jt) {
+        int count=jt.update("delete from account where acc_id=?",
+                acc.getAcc_id());
+        return count;
     }
     }
 
