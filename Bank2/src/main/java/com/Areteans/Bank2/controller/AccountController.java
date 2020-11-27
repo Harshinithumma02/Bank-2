2ea14ea41a8f2a1f528e1151ae2107a6b62809d2
Harshinithumma02 <73868560+Harshinithumma02@users.noreply.github.com>
2020-11-27 09:52:04 +0530

    Add files via upload
    Updations and Deletions JPA

--- Bank2/src/main/java/com/Areteans/Bank2/controller/AccountController.java
+++ Bank2/src/main/java/com/Areteans/Bank2/controller/AccountController.java
@@ -2,45 +2,85 @@ package com.Areteans.Bank2.controller;
 
 import com.Areteans.Bank2.models.Account;
 import com.Areteans.Bank2.models.Password;
-import com.Areteans.Bank2.service.AccountService;
+import com.Areteans.Bank2.models.PaymentJPA;
+import com.Areteans.Bank2.repository.AccountService;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 public class AccountController {
     private final JdbcTemplate jt;
     private final AccountService as;
-
-    public AccountController(JdbcTemplate jt, AccountService as) {
+   public AccountController(JdbcTemplate jt, AccountService as) {
         this.jt = jt;
         this.as = as;
     }
 
-    @PostMapping (path = "updatePassword", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "updatePassword", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String updatePassword(@RequestBody Password ps) {
         System.out.println("in updatePassword endpoint");
-        int op = as.password(ps,jt);
-        return "New Password Updated for " + ps.getUsername()+","+op+"rows updated";
+        int op = as.password(ps, jt);
+        return "New Password Updated for " + ps.getUsername() + "," + op + "rows updated";
     }
-    @GetMapping(path="fromDB/{username}")
+
+    @GetMapping(path = "fromDB/{username}")
     public Account fromDB(@PathVariable("username") String username)
+
     {
-        return as.readFromDB(username,jt);
+        Account acc=as.readFromDB(username,jt);
+        return acc;
     }
 
     @PutMapping(path = "putAccount", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String putAccount(@RequestBody Account acc) {
         System.out.println("in credit reading requestBody");
-        int op = as.putAccount(acc,jt);
-        return "Put successful"+op+" rows put";
+        int op = as.putAccount(acc, jt);
+        return "Put successful" + op + " rows put";
     }
+
     @DeleteMapping(path = "delete", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String delete(@RequestBody Password ps) {
         System.out.println("in delete endpoint");
-        int count = as.delete(ps,jt);
+        int count = as.delete(ps, jt);
         if (count > 0)
             return "deleted for" + ps.getUsername();
         else
             return "Nothing to delete";
+    }
+
+    @PostMapping(path = "account", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public PaymentJPA createAccount(@RequestBody PaymentJPA btjpa)
+    {
+        return as.saveAccount(btjpa);
+    }
+
+    @GetMapping(path = "example", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<PaymentJPA> example()
+    {
+        List<PaymentJPA> list=as.getAll();
+        return list;
+    }
+    @PutMapping(path = "save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<PaymentJPA> example1(@RequestBody PaymentJPA paymentJPA)
+    {
+      return as.save(paymentJPA);
+
+    }
+    @DeleteMapping(path = "delete1", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String example2(@RequestBody PaymentJPA paymentJPA)
+    {
+
+        return as.deleteID(paymentJPA.getTrans_id());
+        //return op;
+    }
+    @PutMapping(path = "addAmount", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String addAmount(@RequestBody PaymentJPA paymentJPA)
+    {
+
+        return as.addAmount(paymentJPA.getTrans_id(),paymentJPA.getAmount());
+        //return op;
     }
 }
