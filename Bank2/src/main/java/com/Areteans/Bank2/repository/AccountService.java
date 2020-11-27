ee930c6b9d1c9348ad4ffb9f8730aef69a46f5c0
Harshinithumma02 <73868560+Harshinithumma02@users.noreply.github.com>
2020-11-27 16:54:23 +0530

    Add files via upload
    commit 2

--- Bank2/src/main/java/com/Areteans/Bank2/repository/AccountService.java
+++ Bank2/src/main/java/com/Areteans/Bank2/repository/AccountService.java
@@ -1,20 +1,23 @@
 package com.Areteans.Bank2.repository;
 
 import com.Areteans.Bank2.models.Account;
-import com.Areteans.Bank2.models.BankTransactionJPA;
 import com.Areteans.Bank2.models.Password;
+import com.Areteans.Bank2.models.PaymentJPA;
 import com.Areteans.Bank2.service.AccountRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class AccountService {
-    private final BankTransactionRepository bankRepo;
+    private final PaymentRepository paymentRepo;
+    public JdbcTemplate jt;
 
-    public AccountService(BankTransactionRepository bankRepo) {
-        this.bankRepo = bankRepo;
+    public AccountService(PaymentRepository paymentRepo) {
+        this.paymentRepo = paymentRepo;
     }
-
 
     public int password(Password ps, JdbcTemplate jt) {
         int count = jt.update("update account set password=? where username=?"
@@ -44,7 +47,47 @@ public class AccountService {
         return count;
     }
 
-    public BankTransactionJPA saveAccount(BankTransactionJPA xyz) {
-        return bankRepo.save(xyz);
+    public PaymentJPA saveAccount(PaymentJPA xyz) {
+        return paymentRepo.save(xyz);
     }
+    public List<PaymentJPA> getAll() {
+        return paymentRepo.findAll();
+    }
+
+
+public String deleteID(int trans_id )
+{
+   int records=paymentRepo.deleteId(trans_id);
+   return  records+"  rows Deleted";
+}
+    public String addAmount(int trans_id ,int amount)
+    {
+        int records=paymentRepo.addAmount(trans_id,amount);
+        return  records+"  rows updated";
+    }
+    public List<PaymentJPA> save(PaymentJPA paymentJPA)
+    {
+        PaymentJPA paymentJPA1=paymentRepo.save(paymentJPA);
+        List<PaymentJPA> paymentJPA2=paymentRepo.findAll();
+        return paymentJPA2;
+    }
+    public Map<String,Object> create (Account acc,JdbcTemplate jt){
+        System.out.println("Hi I am Sushma");
+        Map<String,Object> map= jt.queryForMap("insert into account values (?,?,?,?,?) returning account",
+                acc.getAcc_id(),
+                acc.getAccType(),
+                acc.getBalance(),
+                acc.getUsername(),
+                acc.getPassword());;
+        System.out.println("Hi I am Sushma");
+
+int count1=jt.update("insert into branch(id,address)values (?,?)",
+        acc.getBb().getId(),
+        acc.getBb().getAddress());
+        System.out.println("Hi I am Sushma");
+
+return map;
+    }
+
+
 }
